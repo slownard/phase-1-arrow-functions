cfc9f3f9417b9bd3d47a503cf66a951c031d6bc6
Bernard Legaspi <blegaspi26@gmail.com>
2022-09-21 17:23:21 -0400

    commit tree 1bd48ca1ace87cca8d091ab28cbd044648eac471

---
+++ index.js
@@ -0,0 +1,17 @@
+//arrow functions 
+
+/*
+const playMusic = function (music) {
+    return "Playing some " + music;
+};
+playMusic("Jazz"); */
+
+function divide() {
+
+    return 2000 / 100;
+
+}
+
+const square = x => x ** 2
+
+const add = (a, b) => a + b
